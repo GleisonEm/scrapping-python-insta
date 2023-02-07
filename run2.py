ac5b4170cd4a1f80d07deaaf135461d15683e8a1
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2023-02-07 18:00:39 -0300

    first commit

---
+++ run2.py
@@ -0,0 +1,116 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from decouple import config
+
+# Setup a .env file in the current directory
+# like
+# iguser=yourusername
+# igpassword=yourpsw
+# ==========================================
+USERNAME = config("iguser")
+PASSWORD = config("igpassword")
+# ==========================================
+
+TIMEOUT = 15
+
+
+def scrape():
+    # usr = input('[Required] - Whose followers do you want to scrape: ')
+
+    # user_input = int(
+    #     input('[Required] - How many followers do you want to scrape (60-500 recommended): '))
+    usr = 'gemanuel1208'
+    user_input = 500
+    # options = webdriver.ChromeOptions()
+    # #options.add_argument("--headless")
+    # options.add_argument('--no-sandbox')
+    # options.add_argument("--log-level=3")
+    # mobile_emulation = {
+    #     "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
+    # options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+    # bot = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    bot = webdriver.Firefox()
+
+    bot.get('https://www.instagram.com/')
+
+    time.sleep(1)
+
+    # #accept cookie policy
+    # bot.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    # bot.find_element(By.XPATH,"/html/body/div[4]/div/div/div[3]/div[2]/button").click()
+
+    print("[Info] - Logging in...", USERNAME, PASSWORD)
+
+    # elements = bot.find_element(By.TAG_NAME,"input")
+    # for e in elements:
+    #     print(e)
+    user_element = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')))
+
+    user_element.send_keys(USERNAME)
+
+    pass_element = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')))
+
+    pass_element.send_keys(PASSWORD)
+
+    login_button = WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((
+            By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button')))
+
+    time.sleep(0.4)
+
+    login_button.click()
+
+    time.sleep(10)
+
+    bot.get('https://www.instagram.com/{}/followers/'.format(usr))
+
+    # time.sleep(3.5)
+
+    # WebDriverWait(bot, TIMEOUT).until(
+    #     EC.presence_of_element_located((
+    #         By.XPATH, "//a[contains(@href, '/following')]"))).click()
+
+    # time.sleep(2)
+
+    print('[Info] - Scraping...')
+
+    users = set()
+
+    for _ in range(round(user_input // 20)):
+
+        ActionChains(bot).send_keys(Keys.END).perform()
+
+        time.sleep(1)
+
+    followers = bot.find_elements(By.XPATH,
+    "//a[contains(@href, '/')]")
+
+    # Getting url from href attribute
+    for i in followers:
+        if i.get_attribute('href'):
+            users.add(i.get_attribute('href').split("/")[3])
+        else:
+            continue
+
+    print('[Info] - Saving...')
+    print('[DONE] - Your followers are saved in followers.txt file!')
+
+    with open('followers.txt', 'a') as file:
+        file.write('\n'.join(users) + "\n")
+
+
+if __name__ == '__main__':
+    scrape()
